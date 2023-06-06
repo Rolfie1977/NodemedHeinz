441fc6eaf66624d68e00ed25bd90b15fc59d68e3
Rolf M <rolfie@outlook.dk>
2023-06-06 11:27:23 +0200

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+
 import express from "express";
 
 const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT;
+const dbhost = process.env.DBHOST;
 
 app.get("/", (req, res) => {
 	res.send("Hej verden!");
@@ -14,6 +21,8 @@ app.get("/contact", (req, res) => {
 	res.send("Dette er kontakt siden...");
 });
 
-app.listen(4242, () => {
+app.listen(port, () => {
 	console.log("Express server kører....");
 });
+
+
